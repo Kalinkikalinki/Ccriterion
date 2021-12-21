612686676865820ee5a8ea47c798814370af20dd
Kalinkikalinki <85213344+Kalinkikalinki@users.noreply.github.com>
2021-12-22 00:40:06 +0300

    interface upload

---
+++ window.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from subprocess import call
+import winsound
+from func import stop
+import cv2
+import mediapipe as mp
+import numpy as np
+root=Tk()
+root.geometry('200x100')
+frame = Frame(root)
+frame.pack(pady=20,padx=20)
+
+def Open():
+    call(["python", "main.py"])
+
+btn=Button(frame,text='Open File',command=Open)
+btn.pack()
+
+root.mainloop()
